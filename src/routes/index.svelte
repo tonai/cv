a3c084c2fca3961cf08eb0295b52c8b55b3974f1
Tony Cabaye <tonai59+github@gmail.com>
2020-12-20 20:44:41 +0100

    chore: add husky, lint-staged, prettier and commitlint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,13 @@
 <script>
-
+  export let value;
 </script>
 
 <style>
-
 </style>
 
 <svelte:head>
-	<title>Curriculum Vitæ Tony Cabaye</title>
+  <title>Curriculum Vitæ Tony Cabaye</title>
 </svelte:head>
 
 <h1>Curriculum Vitæ</h1>
+<input type="text" {value} />
